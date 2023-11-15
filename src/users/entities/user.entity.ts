194774baa2a3f727f105f98822ea19e554927879
maulanadata <crypton1001@gmail.com>
2023-11-15 13:48:57 +0700

    first commit

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,50 @@
+import { Level } from '#/levels/entities/level.entity';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+  VersionColumn,
+  CreateDateColumn,
+  ManyToOne,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column({ default: true })
+  isActive: boolean;
+
+  @CreateDateColumn({
+    type: 'timestamp with time zone',
+    nullable: false,
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp with time zone',
+    nullable: false,
+  })
+  updatedAt: Date;
+
+  @DeleteDateColumn({
+    type: 'timestamp with time zone',
+    nullable: true,
+  })
+  deletedAt: Date;
+
+  @VersionColumn()
+  version: number;
+
+  @ManyToOne(() => Level, (level) => level.user)
+  level: Level;
+}
